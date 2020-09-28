8c8a9160e87f4b10c6ca66012979dff7d58254f5
Magnus Bahn Berger <magn351i@edu.easj.dk>
2020-09-28 10:41:04 +0200

    Done

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,17 +5,86 @@ new Vue({
     el: "#app",
     data: {
         name: "",
-        greeting: ""
+        operation: "",
+        output: ""
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
+        manipulateString() {
+            let operation: string = this.operation;
+            let output: string = "";
+            let name: string = this.name;
+            console.log(operation);
+
+            switch (operation) {
+                case "toUpperCase": output = name.toUpperCase(); break;
+                case "toLowerCase": output = name.toLowerCase(); break;
+                case "invertCase": output = invertCase(name); break;
+                case "reverse": output = reverseString(name); break;
+                case "meme": output = meme(name); break;
+                case "toCharNumbers": output = toCharNumbers(name); break;
             }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+
+            this.output = output;
         }
     }
 })
+
+function reverseString(str: string): string {
+    let output: string = "";
+
+    for (let index = str.length - 1; index >= 0; index--) {
+        output = output + str[index];
+    }
+
+    return output;
+}
+
+function meme(str: string): string {
+    let output: string = "";
+    if (str.length > 0) {
+        str = str.toUpperCase();
+
+        for (let index = 0; index < str.length - 1; index++) {
+            output = output + str[index] + " ";
+        }
+
+        output = output + str[str.length - 1]
+    }
+
+    return output;
+}
+
+function toCharNumbers(str:string):string {
+    let output = "";
+
+    if(str.length>0){
+        for (let index = 0; index < str.length - 1; index++) {
+            output = output + str[index].charCodeAt(0) + " ";
+        }
+    
+        output = output + str[str.length - 1].charCodeAt(0);
+    }
+
+    return output;
+}
+
+function invertCase(str:string):string {
+    let output = "";
+
+    for (let index = 0; index < str.length; index++) {
+        let charCode:number = str.charCodeAt(index);
+
+        if (charCode >= 65 && charCode <= 90) {
+            output = output + String.fromCharCode(charCode + 32);
+        }
+        else if (charCode >= 97 && charCode <= 122) {
+            output = output + String.fromCharCode(charCode - 32);
+        }
+        else {
+            output = output + str[index];
+        }
+        
+    }
+
+    return output;
+}
